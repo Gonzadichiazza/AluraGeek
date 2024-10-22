2812300f619415ae54b14a4ebc01a2cea91d827b
Gonzalo Dichiazza <gonzalodichiazza2@gmail.com>
2024-10-22 00:00:49 -0300

    Al editar un producto recupera los datos desde db.js

--- js/agregarProductos.js
+++ js/agregarProductos.js
@@ -1,38 +1,50 @@
 import { conexion } from "./conexion.js";
 
-
-
 const volver = document.querySelector(".volver");
 const formulario = document.querySelector("[data-formulario]");
-async function agregar (e) {
 
+async function agregar(e) {
     try {
-        console.log("hola")
-        e.preventDefault()
-        const nombre = document.querySelector("[data-nombre]").value
-        const precio = document.querySelector("[data-precio]").value
-        const imagen = document.querySelector("[data-imagen]").value
-            
-            await conexion.crearArticulo(nombre, precio, imagen); 
-        
-            window.location.href="../index.html"
+        console.log("hola");
+        e.preventDefault();
 
+        const nombre = document.querySelector("[data-nombre]").value;
+        const precio = document.querySelector("[data-precio]").value;
+        const imagen = document.querySelector("[data-imagen]").value;
+        const errorNombre = document.querySelector("[data-error_nombre]");
+        const errorPrecio = document.querySelector("[data-error_precio]");
+        const errorImagen = document.querySelector("[data-error_imagen]");
 
-        
+        // Resetear mensajes de error
+        errorNombre.setAttribute('hidden', '');
+        errorPrecio.setAttribute('hidden', '');
+        errorImagen.setAttribute('hidden', '');
+
+        if (nombre === '') {
+            errorNombre.removeAttribute('hidden');
+            errorNombre.innerHTML = "Debes colocar un nombre";
+        } else if (precio === '') {
+            errorPrecio.removeAttribute('hidden');
+            errorPrecio.innerHTML = "Debes colocar un precio";
+        } else if (isNaN(precio)) {
+            errorPrecio.removeAttribute('hidden');
+            errorPrecio.innerHTML = "Debes colocar un valor numerico";
+        } else if (imagen === '') {
+            errorImagen.removeAttribute('hidden');
+            errorImagen.innerHTML = "No agregaste ninguna foto";
+        } else {
+            await conexion.crearArticulo(nombre, precio, imagen);
+            window.location.href = "../index.html";
+        }
     } catch (error) {
         console.log(error);
     }
-
-  
 }
 
-function volverAtras(e){
-    
-    e.preventDefault()
-    window.location.href="../index.html"
-
+function volverAtras(e) {
+    e.preventDefault();
+    window.location.href = "../index.html";
 }
 
-volver.addEventListener("click", e => volverAtras(e))
-
-formulario.addEventListener("submit", e => agregar(e))
+volver.addEventListener("click", volverAtras);
+formulario.addEventListener("submit", agregar)
--- js/editarProductos.js
+++ js/editarProductos.js
@@ -1,19 +1,60 @@
 import { conexion } from "./conexion.js"
 const formulario = document.querySelector("[data-formulario]"); 
+const nombre = document.querySelector("[data-nombre]"); 
+const precio = document.querySelector("[data-precio]"); 
+const imagen = document.querySelector("[data-imagen]"); 
 const volver = document.querySelector(".volver")
+const url = new URLSearchParams(window.location.search)
+const id = url.get("id");
+
+async function atributo(e){
+    e.preventDefault(); 
+    const productos =Array.from(await conexion.productos()); 
+    productos.forEach(producto => {
+        if(producto.id == id){
+            console.log(producto.nombre.toString());
+            nombre.setAttribute("value", producto.nombre); 
+            precio.setAttribute("value", producto.precio);  
+            const imagenUrl = producto.imagen.replace("./assets/", "");
+            imagen.setAttribute("value", imagenUrl); 
+
+        }
+    }) 
+
+}
 async function editar (e){
     try {
         e.preventDefault()
-        const url = new URLSearchParams(window.location.search)
-        const id = url.get("id")
-    
-        const nombre = document.querySelector("[data-nombre]").value; 
-        const precio = document.querySelector("[data-precio]").value; 
-        const imagen = document.querySelector("[data-imagen]").value; 
-    
+
         
-       await conexion.editarArticulo(id, nombre, precio, imagen);
-       window.location.href="../index.html";
+    
+        const errorNombre = document.querySelector("[data-error_nombre]");
+        const errorPrecio = document.querySelector("[data-error_precio]");
+        const errorImagen = document.querySelector("[data-error_imagen]");
+
+        errorNombre.setAttribute('hidden', '');
+        errorPrecio.setAttribute('hidden', '');
+        errorImagen.setAttribute('hidden', '');
+        
+        if (nombre.value === '') {
+            errorNombre.removeAttribute('hidden');
+            errorNombre.innerHTML = "Debes colocar un nombre";
+        } else if (precio.value === '') {
+            errorPrecio.removeAttribute('hidden');
+            errorPrecio.innerHTML = "Debes colocar un precio";
+        } else if (isNaN(precio.value)) {
+            errorPrecio.removeAttribute('hidden');
+            errorPrecio.innerHTML = "Debes colocar un valor numerico";
+        } else if (imagen.value === '') {
+            errorImagen.removeAttribute('hidden');
+            errorImagen.innerHTML = "No agregaste ninguna foto";
+        } else {
+            await conexion.editarArticulo(id, nombre.value, precio.value, imagen.value);
+            window.location.href="../index.html";
+        }
+
+
+       
     } catch (error) {
         console.log(error);
     }    
@@ -25,3 +66,4 @@ function volverIndex(e){
 }
 volver.addEventListener("click", e => volverIndex(e))
 formulario.addEventListener("submit", e => editar(e))
+window.addEventListener("DOMContentLoaded", e => atributo(e))
